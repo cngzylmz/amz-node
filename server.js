e32092d64d8816ffb90d19f976a4f57daf5ca2e7
cengizyilmaz <350462Cngz>
2021-10-18 23:36:49 +0300

    .

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ const db = require('./db/mongodb')
 db()
 
 console.log(`Listening On http://localhost:${port}`)
-app.use(cors())
+// app.use(cors())
 app.use('/', require('./api/routers'))
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-})
+// app.use((req, res, next) => {
+//     const error = new Error('Not found');
+//     error.status = 404;
+//     next(error);
+// })
 
 // app.use('/assigns', require('./api/routers'))
 
